2b63f292fce8a00dcc9cf7656d76153a027b9c94
JjangSW <jjang6251@gmail.com>
2026-01-21 16:10:33 +0900

    account 메소드 활용법

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,13 +1,56 @@
 package accounts
 
+import (
+	"errors"
+	"fmt"
+)
+
 //Account struct
 type Account struct {
 	owner string
 	balance int
 }
 
+var errNoMoney = errors.New("Can't withdraw")
+
 //NewAccount creates Account
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// Deposit x amount on your account
+// 리시버로 받아올 경우 포인터를 붙이지 않으면 복사본을 가져오는 것과 같고 원본에는 영향을 주지 않는다.
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// Withdraw x amount from your account
+// 함수의 리턴값을 error로 뒀을 경우 최종적으로 return nil을 해주어야한다.
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMoney
+	}
+	a.balance -= amount
+	return nil
+}
+
+// ChangeOwner of the account
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+// Owner of the account
+func (a Account) Owner() string {
+	return a.owner
+}
+
+// Struct 내부에서 호출되던 것을 커스터마이징해서 원하는 형태로 출력이 가능하다.
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account.\nHas: ", a.Balance())
 }
--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"study/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("nico")
-
-	fmt.Println(account)
+	account.Deposit(10)
+	err := account.Withdraw(11)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(account.Balance(), account.Owner())
 }
