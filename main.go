9dc1a744abb7c040a83f2cb98a5bb6ecea4619ad
JjangSW <jjang6251@gmail.com>
2026-01-22 16:54:50 +0900

    Url Checker

--- main.go
+++ main.go
@@ -1,13 +1,55 @@
 package main
 
 import (
-	"study/mydict"
+	// "study/mydict"
+	"errors"
 	"fmt"
+	"net/http"
 )
 
 // "fmt"
 // "log"
 // "study/accounts"
+
+var errRequestFailed = errors.New("Request failed")
+
+func testUrl() {
+	//var results = map[string]string{}
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
+	}
+	results["Gello"] = "Hello"
+	for _, url := range urls {
+		result := "OK"
+		err := hitUrl(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitUrl(url string) error {
+		fmt.Println("Checking:", url)
+		resp, err := http.Get(url)
+		if err != nil || resp.StatusCode >= 400 {
+			fmt.Println(err)
+			return errRequestFailed
+		}
+		return nil
+}
 
 func main() {
 	// account := accounts.NewAccount("nico")
@@ -42,14 +84,16 @@ func main() {
 	// word, _ := dictionary.Search(baseWord)
 	// fmt.Println(word)
 
-	dictionary := mydict.Dictionary{}
-	baseWord := "hello"
-	dictionary.Add(baseWord, "First")
-	dictionary.Search(baseWord)
-	dictionary.Delete(baseWord)
-	word, err := dictionary.Search(baseWord)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(word)
+	// dictionary := mydict.Dictionary{}
+	// baseWord := "hello"
+	// dictionary.Add(baseWord, "First")
+	// dictionary.Search(baseWord)
+	// dictionary.Delete(baseWord)
+	// word, err := dictionary.Search(baseWord)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(word)
+
+	testUrl();
 }
