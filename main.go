738985e01772ce5f48ec857ca08e126f2c52aa66
JjangSW <jjang6251@gmail.com>
2026-01-22 18:25:11 +0900

    GoRoutine + Channels

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 // "fmt"
 // "log"
 // "study/accounts"
 
+// UrlChecker
 var errRequestFailed = errors.New("Request failed")
 
 func testUrl() {
@@ -42,13 +44,72 @@ func testUrl() {
 }
 
 func hitUrl(url string) error {
-		fmt.Println("Checking:", url)
-		resp, err := http.Get(url)
-		if err != nil || resp.StatusCode >= 400 {
-			fmt.Println(err)
-			return errRequestFailed
-		}
-		return nil
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err)
+		return errRequestFailed
+	}
+	return nil
+}
+
+// GoRoutine
+
+//순차적 실행
+func testSequential() {
+	sexyCount("nico")
+	sexyCount("flynn")
+}
+
+//병렬 실행(GoRoutine)
+func testGo() {
+	go sexyCount("nico")
+	sexyCount("flynn")
+}
+
+//병렬 실행 실패
+func goFail() {
+	go sexyCount("nico")
+	go sexyCount("flynn")
+
+	//이런 식으로 작성하면 main에는 실행되는 함수가 없으므로 비동기 처리 즉 GoRoutine이 실행이 되지 않고 바로 종료가 된다.
+}
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
+	}
+}
+
+
+//---------------------------------------------
+//Chanels
+//GoRoutine 간의 데이터를 주고 받기 위해서는 채널을 생성하고 채널을 통해 주고 받는다.
+func testChanels () {
+	c := make(chan bool)
+	people := [2]string{"nico", "flynn"}
+	for _, person := range people {
+		go isSexy(person, c)
+	}
+	
+	//time sleep이 없더라도 Go에서는 채널에서 응답값을 받기 위해 기다린다.
+	// result := <-c
+	// fmt.Println(result)
+	//fmt.Println(<-c) //위와 같음
+	//fmt.Println(<-c)
+
+	//사람 2명에 대한 채널 수신 후 한번 더 호출 시 deadlock 발생
+	//fmt.Println(<-c)
+
+	// <- 이런 식으로 채널로부터 수신한다는 뜻은 Blocking Operation이라고 할 수 있다.
+	for i:=0; i < len(people); i++ {
+		fmt.Println(<-c)
+	}
+
+}
+func isSexy(person string, c chan bool) {
+	time.Sleep(time.Second * 5)
+	c <- true
 }
 
 func main() {
@@ -95,5 +156,6 @@ func main() {
 	// }
 	// fmt.Println(word)
 
-	testUrl();
+	//testUrl();
+	testGo()
 }
